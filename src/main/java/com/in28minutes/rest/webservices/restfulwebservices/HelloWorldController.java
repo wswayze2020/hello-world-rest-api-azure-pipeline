4dd07900cf1ea5847354eafb8f597fe42efbed94
Wallace Swayze <841626@corpaa.aa.com>
2020-11-05 11:49:42 -0600

    second commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World V2";
+		return "Hello World V10";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
